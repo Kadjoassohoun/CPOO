8459ce57dad9b0eab10267f88ddfa217b2018ff5
Kadjoassohoun <32166865+Kadjoassohoun@users.noreply.github.com>
2017-11-15 22:12:04 +0100

    Update Main.java

--- TD_SalleCinema/src/main/Main.java
+++ TD_SalleCinema/src/main/Main.java
@@ -1,29 +1,40 @@
 package main;
 
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.util.logging.Handler;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.io.IOException;
+import java.util.logging.SimpleFormatter;
 
-import model.SalleCinema;
+import model.impl.SalleCinema;
+import model.exceptions.SallePleine;
+import model.fact.SalleFactory;
 
 public final class Main {
-    
+    private static final Logger LOG = Logger.getLogger(Main.class.getCanonicalName());
 
     public static void main (String[] args) {
 
-        SalleCinema a = new SalleCinema();
+        SalleFactory sf = new SalleFactory();
 
-        int nbPlace = 1000;
+        SalleCinema s1 = new SalleCinema();
+        SalleCinema s2 = sf.createSalleCinema("Fast and Furious 8",500,14.7);
 
-        int nbPlaceVendues = 455;
-
-        double prix = 100.0;
-
-        a.vendrePlace();
-        a.tauxRemplissage();
-        a.estPleine();
-        a.nbPlacesDisponibles();
-        a.ChiffresdAffaires();
         
-        
-
-        System.out.println(a.toString());
+        try{
+            int i;
+            for(i = 0; i <= 600; i++){
+                s2.vendrePlace();
+            }
+        }catch(SalleEstPleine ex){
+            LOG.severe(ex.getMessage());
+        }finally{
+            LOG.fine("Il reste encore " + s2.nbPlacesDisponible());
+        }
+            
+        System.out.println(s1);    
+        System.out.println(s2);
     }
 }
